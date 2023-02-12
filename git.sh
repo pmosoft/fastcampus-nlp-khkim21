3e10c0003cdc98203b884b1ffd22016a416cb018
pmosoft <pmosoft@gmail.com>
2023-02-12 12:13:53 +0900

    --

---
+++ git.sh
@@ -0,0 +1,17 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/pmosoft/fastcampus-nlp-khkim21.git
+
+git push -u origin main
+
+git remote add origin https://github.com/pmosoft/fastcampus-nlp-khkim21.git
+git branch -M main
+git push -u origin main
+
+git add *.*
+git commit -m "--"
+
+
+git push origin +main
